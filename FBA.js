f1b3dda277eaf83e2f3c45d436b590fd4041a43c
omnidune <52822149+omnidune@users.noreply.github.com>
2021-05-03 23:14:18 +0530

    Updated script
    according to the new FBA page redesign

--- FBA.js
+++ FBA.js
@@ -1,26 +1,90 @@
 // ==UserScript==
-// @name         FBA Discrepancy
+// @name         FBA Mod
 // @namespace    https://raw.githubusercontent.com/omnidune/UserScripts/master/FBA.js
-// @version      0.2
-// @description  FBA Discrepancy detector
+// @version      0.3
+// @description  FBA Page Mod
 // @author       Raj
 // @match        https://sellercentral.amazon.com/gp/fba/inbound-queue/index.html*
 // @downloadURL  https://raw.githubusercontent.com/omnidune/UserScripts/master/FBA.js
 // @grant        none
 // ==/UserScript==
 
+function generateTable() {
+    document.querySelector("#intro_message").innerHTML += "<br><br>";
+    var table = document.createElement("table");
+    document.querySelector("#intro_message").appendChild(table);
+    let cell;
+    let element;
+    for (let index = 0; index < 50; index++) {
+        let row = table.insertRow();
+        cell = row.insertCell();
+        element = document.querySelectorAll("#shipment-name-container > kat-link > a")[0];
+        cell.appendChild(element);
+        
+        cell = row.insertCell();
+        element = document.querySelectorAll('#shipment_id_text')[index];
+        cell.appendChild(element);
+        
+        cell = row.insertCell();
+        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(2) > div:nth-child(1)')[index];
+        cell.appendChild(element);
+        
+        cell = row.insertCell();
+        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(3) > div:nth-child(1)')[index];
+        cell.appendChild(element);
+        
+        cell = row.insertCell();
+        element = document.querySelectorAll('#numeric-cell > kat-link > a')[0];
+        cell.appendChild(element);
+        
+        cell = row.insertCell();
+        element = document.querySelectorAll('#numeric-cell > div:nth-child(1)')[2];
+        cell.appendChild(element);
+
+        cell = row.insertCell();
+        element = document.querySelectorAll('#received_quantity_text')[index];
+        cell.appendChild(element);
+
+        cell = row.insertCell();
+        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(4)')[index];
+        cell.appendChild(element);
+
+        cell = row.insertCell();
+        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(6) > kat-badge > span')[0];
+        cell.appendChild(element);
+    }
+}
+
 function mark() {
-    var elementLengh = document.getElementById('fba-inbound-queue-shipments-collection').getElementsByTagName('tr').length;
+    var elementLengh = document.querySelectorAll("#content-row").length;
 
-
-    for (var i = 1; i <= elementLengh; i++) {
-        var a = document.querySelector("#fba-inbound-queue-shipments-collection > tr:nth-child(" + i + ") > td:nth-child(6)").textContent;
-        var b = document.querySelector("#fba-inbound-queue-shipments-collection > tr:nth-child(" + i + ") > td:nth-child(7)").textContent;
-
+    for (var i = 0; i <= elementLengh; i++) {
+        var a = document.querySelectorAll('#numeric-cell > div:nth-child(1)')[i + 2].textContent;
+        var b = document.querySelectorAll("#received_quantity_text")[i].textContent;
+        //console.log(a, b);
         if (a - b > 0) {
-            document.querySelector("#fba-inbound-queue-shipments-collection > tr:nth-child(" + i + ")").style.background = "#ffa1a1";
+            document.querySelectorAll("#content-row")[i].style.background = "#ffa1a1";
         }
     }
 }
 
-document.querySelector("#fba-inbound-queue-page").addEventListener("click", mark);
+var existCondition = setInterval(function() {
+    if (document.querySelector("#filter-export-container")) {
+        clearInterval(existCondition);
+        var button = document.createElement("button");
+        button.innerHTML = "Generate HTML Table";
+
+        var body = document.querySelector("#filter-export-container");
+        body.appendChild(button);
+
+        button.addEventListener("click", generateTable);
+
+        /////////////////////////////////////////
+        button = document.createElement("button");
+        button.innerHTML = "Mark Discrepant Shipments";
+
+        body = document.querySelector("#filter-export-container");
+        body.appendChild(button);
+        button.addEventListener("click", mark);
+    }
+}, 100);
