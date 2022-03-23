ba3cbf97dd4f363117d15a97589718433d9beef8
omnidune <52822149+omnidune@users.noreply.github.com>
2022-03-23 20:27:33 +0530

    Update FBA.js
    - HTML table is independent of the main table
    - HTML table updates with the generate button
    - HTML table length is now the same as the main table.

--- FBA.js
+++ FBA.js
@@ -1,58 +1,85 @@
 // ==UserScript==
 // @name         FBA Mod
 // @namespace    https://raw.githubusercontent.com/omnidune/UserScripts/master/FBA.js
-// @version      1.0
+// @version      2.0
 // @description  FBA Page Mod
 // @author       Raj
 // @match        https://sellercentral.amazon.com/gp/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @downloadURL  https://raw.githubusercontent.com/omnidune/UserScripts/master/FBA.js
+// @updateURL    https://raw.githubusercontent.com/omnidune/UserScripts/master/FBA.js
 // @grant        none
 // ==/UserScript==
 
 function generateTable() {
-    document.querySelector("#intro_message").innerHTML += "<br><br>";
+    try {
+        document.querySelector("#intro_message > br:nth-child(1)").remove()
+        document.querySelector("#intro_message > br:nth-child(1)").remove()
+        document.querySelector("#intro_message > table").remove()
+    }
+    catch {}
+
+    let gap = document.querySelector("#intro_message").innerHTML += "<br><br>";
     var table = document.createElement("table");
     document.querySelector("#intro_message").appendChild(table);
     let cell;
     let element;
-    for (let index = 0; index < 50; index++) {
+    //
+    let matrix = document.querySelector("#main-table > kat-table > kat-table-body");
+    //Search from the main table only, not the HTML table
+    let length = matrix.querySelectorAll('[id=shipment-name-container]').length
+
+    for (let index = 0; index < length; index++) {
+        //
+        let matrix = document.querySelector("#main-table > kat-table > kat-table-body");
+        //Search from the main table only, not the HTML table
+        //
         let row = table.insertRow();
         cell = row.insertCell();
-        element = document.querySelectorAll("#shipment-name-container > kat-link > a")[0];
+        element = matrix.querySelectorAll('[id=shipment-name-container]')[index].cloneNode(true);
         cell.appendChild(element);
+        //FBA Shipment Title/Link
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#shipment_id_text')[index];
+        element = matrix.querySelectorAll('#shipment_id_text')[index].cloneNode(true);
         cell.appendChild(element);
+        //FBA Shipment ID
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(2) > div:nth-child(1)')[index];
+        element = matrix.querySelectorAll('#content-row > kat-table-cell:nth-child(2) > div:nth-child(1)')[index].cloneNode(true);
         cell.appendChild(element);
+        //Date Created
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(3) > div:nth-child(1)')[index];
+        element = matrix.querySelectorAll('#content-row > kat-table-cell:nth-child(3) > div:nth-child(1)')[index].cloneNode(true);
         cell.appendChild(element);
+        //Last Updated
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#numeric-cell > kat-link > a')[0];
+        element = matrix.querySelectorAll("#numeric-cell > kat-link")[index].cloneNode(true);
         cell.appendChild(element);
+        //Number of SKUs
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#numeric-cell > div:nth-child(1)')[2];
+        element = matrix.querySelectorAll('#numeric-cell > div:nth-child(1)')[index].cloneNode(true);
         cell.appendChild(element);
+        //Units Sent
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#received_quantity_text')[index];
+        element = matrix.querySelectorAll('#received_quantity_text')[index].cloneNode(true);
         cell.appendChild(element);
+        //Units Received
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(4)')[index];
+        element = matrix.querySelectorAll('#content-row > kat-table-cell:nth-child(4)')[index].cloneNode(true);
         cell.appendChild(element);
+        //Ship to
 
         cell = row.insertCell();
-        element = document.querySelectorAll('#content-row > kat-table-cell:nth-child(6) > kat-badge > span')[0];
+        element = matrix.querySelectorAll("#content-row > kat-table-cell:nth-child(7) > kat-badge")[index].cloneNode(true);
         cell.appendChild(element);
+        //Status
+
     }
 }
 
